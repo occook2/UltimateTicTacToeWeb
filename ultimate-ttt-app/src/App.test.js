3ca3711745b9b3614465822a7b7baabbbc8e1314
occook2 <119069061+occook2@users.noreply.github.com>
2024-02-06 20:27:08 -0500

    Attempted to add unit test (not working)

--- ultimate-ttt-app/src/App.test.js
+++ ultimate-ttt-app/src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders App component', () => {
+  const { getByTest } = render(<App />);
+  const titleElement = getByText(/Ultimate Tic-Tac-Toe/i);
+  expect(titleElement).toBeInTheDocument();
 });
