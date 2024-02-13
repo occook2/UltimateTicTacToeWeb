a1f75650cb039b2c2fad1aea2d8e1dc70767ffef
occook2 <119069061+occook2@users.noreply.github.com>
2024-02-12 19:50:31 -0500

    Updated API call

--- ultimate-ttt-app/src/components/BigBoard.js
+++ ultimate-ttt-app/src/components/BigBoard.js
@@ -1,60 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './BigBoard.css'; // Import the CSS file
-import boardState from '../mock-data/boardState.json'
+import apiBoardState from '../mock-data/apiBoardState.json'
 import emptyBoardState from '../mock-data/emptyBoardState.json'
 import Board from './Board';
 
 function BigBoard() {
-    // Helper function to render a single board    
-    const renderBoard = (i, data, boardMove, complete) => {
-      if (i == boardMove || boardMove == -1) {
-        return <Board key = {i} boardNumber = {i} boardData = {data} boardMove = {true} complete = {complete} onSquareClick={handleSquareClick}/>;
-      }
-      else {
-        return <Board key = {i} boardNumber = {i} boardData = {data} boardMove = {false} complete = {complete} onSquareClick={handleSquareClick}/>;
-      }
-      
-    };
-    
-    const handleSquareClick = (address) => {
-      // Simulate API call to fetch data based on the square address
-      // This function will then update the state of BigBoard to reflect the changes
-      console.log("API call triggered for square address: ", address);
+  const [bigBoardState, setBigBoardState] = useState(null)
+
+  // Helper function to render a single board    
+  const renderBoard = (i, data, boardMove, complete) => {
+    if (i == boardMove || boardMove == -1) {
+      return <Board key = {i} boardNumber = {i} boardData = {data} boardMove = {true} complete = {complete} onSquareClick={handleSquareClick}/>;
+    }
+    else {
+      return <Board key = {i} boardNumber = {i} boardData = {data} boardMove = {false} complete = {complete} onSquareClick={handleSquareClick}/>;
+    } 
+  };
+  
+  const handleSquareClick = async (address) => {
+    // Simulate API call to fetch data based on the square address
+    // This function will then update the state of BigBoard to reflect the changes
+    console.log("API call triggered for square address: ", address);
+    setBigBoardState(apiBoardState)
   };
 
     // Helper function to render a row of boards
-    const renderRow = (rowIndex) => {
-      const boards = [];
-      for (let j = 0; j < 3; j++) {
-        const boardKey = rowIndex * 3 + j;
-        boards.push(renderBoard(boardKey, 
-          boardState.bigBoard[boardKey], 
-          boardState.boardMove, 
-          boardState.completeBoards[boardKey]));
-      }
-      
-      return (
-        <div className="big-board-row" key={rowIndex}>
-          {boards}
-        </div>
-      );
-    };
-  
-    // Render the entire BigBoard
-    const renderBigBoard = () => {
-      const rows = [];
-      for (let i = 0; i < 3; i++) {
-        rows.push(renderRow(i));
-      }
-      return <div className="big-board">{rows}</div>;
-    };
-  
+  const renderRow = (rowIndex, boardState) => {
+    const boards = [];
+    for (let j = 0; j < 3; j++) {
+      const boardKey = rowIndex * 3 + j;
+      boards.push(renderBoard(boardKey, 
+        boardState.bigBoard[boardKey], 
+        boardState.boardMove, 
+        boardState.completeBoards[boardKey]));
+    }
+    
     return (
-      <div className='big-board-container'>
-        {renderBigBoard()}
+      <div className="big-board-row" key={rowIndex}>
+        {boards}
       </div>
     );
-  }
+  };
+  
+  // Render the entire BigBoard
+  const renderBigBoard = (boardState) => {
+    const rows = [];
+    for (let i = 0; i < 3; i++) {
+      rows.push(renderRow(i, boardState));
+    }
+    return <div className="big-board">{rows}</div>;
+  };
+  
+  return (
+    <div className='big-board-container'>
+      {bigBoardState !== null ? renderBigBoard(bigBoardState) : renderBigBoard(emptyBoardState)}
+    </div>
+  );
+}
   
   export default BigBoard;
   
