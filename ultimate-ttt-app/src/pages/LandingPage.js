95d3ee8789cd9fdde54cb5d2e4463545ba02ba45
occook2 <119069061+occook2@users.noreply.github.com>
2024-03-10 10:34:53 -0400

    Made Rules Box into its own standalone component

--- ultimate-ttt-app/src/pages/LandingPage.js
+++ ultimate-ttt-app/src/pages/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import RulesBox from '../components/RulesBox.js'; // Import the RulesBox component
 import './LandingPage.css'; // Import the CSS file for styling
 
 function LandingPage() {
@@ -20,25 +21,7 @@ function LandingPage() {
         </Link>
         <button className="rules-button" onClick={toggleRules}>Rules</button>
       </div>
-      {/* Rules Box */}
-      {showRules && (
-        <div className="rules-box">
-          <div className="rules-header">
-            <h2>Rules</h2>
-            <span className="close" onClick={toggleRules}>&times;</span>
-          </div>
-          <div className="rules-content">
-            <ol>
-              <li>Board Setup: Ultimate Tic-Tac-Toe is played on a 3x3 grid, with nine individual 3x3 grids within.</li>
-              <li>Turns: Players take turns placing their symbol (traditionally X or O) on any empty space within one of the small 3x3 grids.</li>
-              <li>Board Selection: When a player makes a move in one of the small grids, their opponent must then make a move in the corresponding small grid on the large board.</li>
-              <li>Winning Condition: The objective is to win the small 3x3 grids. When a player wins a small grid, they get to mark that grid with their symbol (X or O), and it counts as a regular Tic-Tac-Toe win.</li>
-              <li>Block Rule: If a player's next move corresponds to a small grid that has already been won or is full, they can play their next move in any other empty small grid on the large board.</li>
-              <li>End of the Game: The game ends when one player wins three small grids in a row on the larger board or when all small grids are full, resulting in a draw.</li>
-            </ol>
-          </div>
-        </div>
-      )}
+        <RulesBox showRules={showRules} toggleRules={toggleRules} />
     </div>
   );
 }
