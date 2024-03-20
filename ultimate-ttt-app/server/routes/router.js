734f4b0f1575e211cfae367a414e907d53e5bc61
occook2 <119069061+occook2@users.noreply.github.com>
2024-03-20 17:50:52 -0400

    Created Symbol Select Box, Need Backed

--- ultimate-ttt-app/server/routes/router.js
+++ ultimate-ttt-app/server/routes/router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const boardController = require('../controllers/boardController');
+const startController = require('../controllers/startController')
 
 router.post('/move', boardController.handleMove);
+
+router.post('/selectSymbol', startController.selectSymbol);
 
 module.exports = router
