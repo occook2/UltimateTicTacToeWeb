38f25e6473b16bb43d98449eb92613b78bc7ab66
occook2 <occook2@gmail.com>
2024-04-08 11:37:31 -0400

    Created Production server via Docker

--- ultimate-ttt-app/server/index.js
+++ ultimate-ttt-app/server/index.js
@@ -1,23 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const router = require('./routes/router')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const router = require('./routes/router');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const corsOptions = {
     origin: '*',
     credentials: true,
-    optionSuccessStatus:200
-}
+    optionSuccessStatus: 200
+};
 
-app.use(cors(corsOptions))
-app.use('/', router)
+app.use(cors(corsOptions));
+app.use('/', router);
 
-const port = 4000
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Serve the API
+app.get('/api', (req, res) => {
+    res.json({ message: 'Welcome to the Ultimate Tic-Tac-Toe API!' });
+});
+
+// Serve the React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+const port = 4000;
 const server = app.listen(port, () => {
-    console.log('Server is runnning on port ' + port)
-})
+    console.log('Server is running on port ' + port);
+});
